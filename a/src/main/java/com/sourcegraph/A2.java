ad9d4ef129381039611ef41fddb2206610ba088c
Aaron Leung <modus.tollens@gmail.com>
2017-08-09 11:31:10 -0700

    use a dependency of a sibling module

---
+++ a/src/main/java/com/sourcegraph/A2.java
@@ -0,0 +1,8 @@
+package com.sourcegraph;
+
+import org.joda.time.Chronology;
+import org.joda.time.chrono.GregorianChronology;
+
+public class A2 {
+    Chronology chronology = GregorianChronology.getInstance();
+}
